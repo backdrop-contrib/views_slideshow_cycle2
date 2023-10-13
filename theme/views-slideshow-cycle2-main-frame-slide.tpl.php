840724741904716f74a497efffd0217d054e80c9
Martin Price <github@systemhorizons.co.uk>
2023-10-13 16:59:41 +0100

    Minor PHPCS tweak to template files.

--- theme/views-slideshow-cycle2-main-frame-slide.tpl.php
+++ theme/views-slideshow-cycle2-main-frame-slide.tpl.php
@@ -1,3 +1,9 @@
+<?php
+/**
+ * @file
+ * Template for main frame slide for Views Slideshow Cycle2.
+ */
+?>
 <div class="<?php print implode(' ', $classes); ?>">
   <?php print $fields; ?>
 </div>
--- theme/views-slideshow-cycle2-main-frame.tpl.php
+++ theme/views-slideshow-cycle2-main-frame.tpl.php
@@ -1,3 +1,9 @@
+<?php
+/**
+ * @file
+ * Template for main frame for Views Slideshow Cycle2.
+ */
+?>
 <div id="views-slideshow-cycle2-main-frame-<?php print $vss_id; ?>" class="<?php print implode(' ', $classes); ?>" <?php print $data_attr; ?>>
   <?php print $rendered_rows; ?>
 </div>
